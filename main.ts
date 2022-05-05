0eeaf105890f68b9d366504b3218750b1b17c6c8
Reluzapro <98704219+Reluzapro@users.noreply.github.com>
2022-05-05 11:02:18 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,23 +5,23 @@ blockytalky.onReceivedNumber(function (key, value) {
         if (key == "recul") {
             b = value
             if (a > 0 && b > 0) {
-                motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, b * Math.cos(a))
-                motor.MotorRun(motor.Motors.M2, motor.Dir.CW, b)
+                motor.MotorRun(motor.Motors.M1, motor.Dir.CW, b * Math.cos(a))
+                motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, b)
             } else {
                 if (a < 0 && b > 0) {
-                    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, b)
-                    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, b * Math.cos(a))
+                    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, b)
+                    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, b * Math.cos(a))
                 } else {
                     if (a > 0 && b < 0) {
-                        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, b * Math.cos(a))
-                        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, b)
+                        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, b * Math.cos(a))
+                        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, b)
                     } else {
                         if (a < 0 && b < 0) {
-                            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, b)
-                            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, b * Math.cos(a))
+                            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, b)
+                            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, b * Math.cos(a))
                         } else {
-                            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, b)
-                            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, b)
+                            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, b)
+                            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, b)
                         }
                     }
                 }
